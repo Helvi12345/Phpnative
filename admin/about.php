99f64f982a7a0fc8ed9ef68bfcd0f132ca12e707
Helvi12345 <helviayu863@gmail.com>
2023-04-04 20:49:01 +0700

    Tugas web native

---
+++ admin/about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <h1>Selamat Datang di Web pertama saya</h1>
+    <br>
+    <p>Halo perkenalkan saya Helvi Ayu Syahputri, pembuat website ini</p><br>
+    <p>Saya adalah seorang mahasiswi asal kabupaten Langkat, sumatera utara. Yang saat ini menjalani semester 6 di perguruan tinggi negeri yang ada di Aceh, tepatnya di kota Lhokseumawe yaitu Universitas Malikussaleh</p>
+    <p>Saya seorang mahasiswi dengan program studi teknik informatika</p>
+</body>
+
+</html>
---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<?php
+//memanggil file bagian atas
+include_once 'top.php';
+//memanggil dan memproses file bagian menu
+include_once 'menu.php';
+?>
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1>Selamat datang di web admin</h1>
+            <?php
+            error_reporting(0);
+            $url = $_GET['url'];
+            if ($url == 'dashboard') {
+                include_once 'dashboard.php';
+            } else if (!empty($url)) {
+                include_once '' . $url . '.php';
+            } else {
+                'dashboard.php';
+            }
+            ?>
+        </div>
+    </main>
+</div>
+
+<?php
+//memanggil file bagian bottom
+include_once 'bottom.php';
+?>
